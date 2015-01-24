85ce44c9cc213e8b8cb7cd0216e5295b42714d4f
tangwei93 <tw93@qq.com>
2015-01-24 21:46:59 +0800

    grunt集成自动重启

---
+++ gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        watch: {
+            jade: {
+                files: ['views/**'],
+                options: {
+                    livereload: true
+                }
+            },
+            js: {
+                files: ['public/js/**', 'models/**/*.js', 'schemas/**/*.js'],
+                //tasks: ['jshint'],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+        nodemon: {
+            dev: {
+                script: 'app.js',
+                options: {
+                    file: 'app.js',
+                    args: [],
+                    ignoredFiles: ['README.md', 'node_modules/**', '.DS_Store'],
+                    watchedExtensions: ['js'],
+                    watchedFolders: ['app', 'config'],
+                    debug: true,
+                    delayTime: 1,
+                    env: {
+                        PORT: 3000
+                    },
+                    cwd: __dirname
+                }
+            }
+        },
+        concurrent: {
+            tasks: ['nodemon', 'watch'],
+            options: {
+                logConcurrentOutput: true
+            }
+        }
+
+    });
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-nodemon');
+    grunt.loadNpmTasks('grunt-concurrent');
+
+    grunt.option('force', true);
+    grunt.registerTask('default', ['concurrent']);
+};
