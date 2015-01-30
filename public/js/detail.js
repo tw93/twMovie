457c1b48cf7e689705fbd179bc85ce296703e847
tangwei93 <tw93@qq.com>
2015-01-30 11:06:37 +0800

    可以对回复进行回复了

--- public/js/detail.js
+++ public/js/detail.js
@@ -5,15 +5,25 @@ $(function() {
     $('.comment').on('click', function() {
         var commentId = $(this).data('cid');
         var toId = $(this).data('tid');
+        if($('#toId').length>0){
+            $("#toId").val(toId);
+        }else{
         $('<input>').attr({
             type: 'hidden',
+            id:'toId',
             name: 'comment[tid]',
             value: toId
-        }).appendTo('#commentForm');
+        }).appendTo('#commentForm');     
+        }
+       if($('#commentId').length>0){
+            $("#commentId").val(commentId);
+        }else{
         $('<input>').attr({
             type: 'hidden',
+            id: 'commentId',
             name: 'comment[cid]',
             value: commentId
         }).appendTo('#commentForm');
+    }
     })
 });
