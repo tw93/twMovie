29d2af4a50732dddcdba07afb58a9bd26a7a0d2f
tangwei93 <tw93@qq.com>
2015-01-31 15:59:35 +0800

    增加搜索 共用列表

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -22,31 +22,52 @@ exports.index = function(req, res) {
 // search
 exports.search = function(req, res) {
     var catId = req.query.cat;
-    var page = req.query.p;
+    var page = parseInt(req.query.p, 10) || 0;
     var count = 3;
     var index = page * count;
+    var q = req.query.q;
+    if (catId) {
+        Category.find({
+            _id: catId
+        }).populate({
+            path: 'movies',
+            select: 'title poster'
+        }).exec(function(err, categories) {
+            if (err) {
+                console.log(err);
+            }
+            var category = categories[0] || {};
+            var movies = category.movies || {};
+            var results = movies.slice(index, index + count);
 
-    Category.find({
-        _id: catId
-    }).populate({
-        path: 'movies',
-        select:'title poster'
-    }).exec(function(err, categories) {
-        if (err) {
-            console.log(err);
-        }
-        var category = categories[0] || {};
-        var movies=category.movies || {};
-        var results=movies.slice(index,index+count);
-
-        console.log(movies);
-        res.render('results', {
-            title: "twMovie结果列表",
-            movies: results,
-            currentPage: (page - 0 + 1),
-            query: 'cat=' + catId,
-            totalPage: Math.ceil(movies.length / count),
-            keyword: category.name
-        });
-    })
+            console.log(movies);
+            res.render('results', {
+                title: "twMovie结果列表",
+                movies: results,
+                currentPage: (page - 0 + 1),
+                query: 'cat=' + catId,
+                totalPage: Math.ceil(movies.length / count),
+                keyword: category.name
+            });
+        })
+    } else {
+        Movie.find({
+                title: new RegExp(q + '.*', 'i')
+            })
+            .exec(function(err, movies) {
+                if (err) {
+                    console.log(err);
+                }
+                var results = movies.slice(index, index + count);
+                console.log(results);
+                res.render('results', {
+                    title: "twMovie结果列表",
+                    movies: results,
+                    currentPage: (page + 1),
+                    query: 'q=' + q,
+                    totalPage: Math.ceil(movies.length / count),
+                    keyword: q
+                });
+            })
+    }
 }
